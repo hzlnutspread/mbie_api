763feb6b735efdf35230c610046c793783e8c895
hzlnutspread <smithkg293@gmail.com>
2022-07-25 02:16:31 +1200

    Remove redundant DataTwo class. Add additional Data fields. Add Map for preparing parameters for third and final get requests

--- src/Data.java
+++ src/Data.java
@@ -2,7 +2,24 @@ public class Data {
 
     private String tableName;
     private Item[] items;
+    private String areaDefinition;
+    private String periodCovered;
 
+    public String getAreaDefinition() {
+        return areaDefinition;
+    }
+
+    public void setAreaDefinition(String areaDefinition) {
+        this.areaDefinition = areaDefinition;
+    }
+
+    public String getPeriodCovered() {
+        return periodCovered;
+    }
+
+    public void setPeriodCovered(String periodCovered) {
+        this.periodCovered = periodCovered;
+    }
 
     public Item[] getItems() {
         return items;
@@ -11,7 +28,6 @@ public class Data {
     public void setItems(Item[] items) {
         this.items = items;
     }
-
 
     public String getTableName() {
         return tableName;
--- src/RunApi.java
+++ src/RunApi.java
@@ -7,11 +7,13 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Properties;
 
 public class RunApi {
     public static void main(String[] args) throws Exception {
 
+        // load properties file
         Properties defaultProps = new Properties();
         FileInputStream in = new FileInputStream("src/api.properties");
         defaultProps.load(in);
@@ -19,36 +21,38 @@ public class RunApi {
 
         String url = defaultProps.getProperty("URL");
         String token = defaultProps.getProperty("TOKEN");
+        String areaPath = defaultProps.getProperty("AREA_PATH");
+        String statsPath = defaultProps.getProperty("STATS_PATH");
 
         // First API request
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest getAreaCodeReq = HttpRequest.newBuilder()
-                .uri(new URI(url))
+                .uri(new URI(url + areaPath))
                 .header("Authorization", "Bearer " + token)
                 .header("Accept", "text/csv")
                 .build();
         HttpResponse<String> getAreaCodeResp = httpClient.send(getAreaCodeReq, HttpResponse.BodyHandlers.ofString());
 
         Gson gson = new Gson();
-        Data data = gson.fromJson(getAreaCodeResp.body(), Data.class);
+        Data areaCodeData = gson.fromJson(getAreaCodeResp.body(), Data.class);
 
-        ArrayList<String> areaCode = new ArrayList<>();
-        for (Item item : data.getItems()) {
-            areaCode.add(item.code);
+        ArrayList<String> areaCodes = new ArrayList<>();
+        for (Item item : areaCodeData.getItems()) {
+            areaCodes.add(item.code);
         }
-        System.out.println(areaCode);
+        System.out.println(areaCodes);
         Thread.sleep(1000);
 
         // Second API request
         HttpClient httpClientTwo = HttpClient.newHttpClient();
         HttpRequest getRequestTwo = HttpRequest.newBuilder()
-                .uri(new URI(url + "/" + areaCode.get(9)))
+                .uri(new URI(url + areaPath + "/" + areaCodes.get(3)))
                 .header("Authorization", "Bearer " + token)
                 .header("Accept", "application/json")
                 .build();
         HttpResponse<String> getResponseTwo = httpClientTwo.send(getRequestTwo, HttpResponse.BodyHandlers.ofString());
 
-        DataTwo dataTwo = gson.fromJson(getResponseTwo.body(), DataTwo.class);
+        Data dataTwo = gson.fromJson(getResponseTwo.body(), Data.class);
 
         ArrayList<String> codeArrayTwo = new ArrayList<>();
         for (Item item : dataTwo.getItems()) {
@@ -61,5 +65,39 @@ public class RunApi {
             labelArray.add(item.label);
         }
         System.out.println(labelArray);
+
+        // 3rd get request
+        String[] dates = {"2022-01", "2022-02", "2022-03"};
+
+        Map<String, String> params = Map.of( //
+                "period-ending", dates[0], //
+                "num-months", "12", //
+                "area-definition", "TA2019", //
+                 "include-aggregates", "false", //
+                 "statistics", "mean", //
+                "dwelling-type", "BoardingHouse", //
+                 "num-bedrooms", "2");
+
+        ArrayList<String> ar = new ArrayList<>();   // Variable scope notes****
+
+        for (Map.Entry<String, String> param : params.entrySet()) {
+            String pa = param.getKey() + "=" + param.getValue();
+            ar.add(pa);
+        }
+
+        String queryParams = String.join("&", ar);
+
+
+        HttpClient httpClientThree = HttpClient.newHttpClient();
+        HttpRequest getRequestThree = HttpRequest.newBuilder()
+                .uri(new URI(url + statsPath + "?" + queryParams))
+                .header("Authorization", "Bearer " + token)
+                .header("Accept", "application/json")
+                .build();
+        HttpResponse<String> getResponseThree = httpClientThree.send(getRequestThree, HttpResponse.BodyHandlers.ofString());
+
+        Data dataThree = gson.fromJson(getResponseTwo.body(), Data.class);
+
+
     }
 }
